f92873df999b2a85cbc6792b13329808a14f8aaa
Krzysztof <krzysztof@studiovector.pl>
2019-12-28 10:51:40 +0100

    Version 1.1.0 - removed admin requirement; added option for teleporting to player

--- src/main/java/com/github/sejoslaw/teleportme/TeleportMe.java
+++ src/main/java/com/github/sejoslaw/teleportme/TeleportMe.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
+import net.minecraft.command.arguments.EntityArgument;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.server.ServerWorld;
@@ -21,6 +22,7 @@ public class TeleportMe {
     public static final String MODID = "teleportme";
 
     private static final String COMMAND = "tpme";
+    private static final String PLAYER_NAME = "playerName";
     private static final String DIM_ID = "dimensionId";
     private static final String POS_X = "x";
     private static final String POS_Y = "y";
@@ -34,7 +36,9 @@ public class TeleportMe {
     public void serverStarting(FMLServerStartingEvent event) {
         LiteralArgumentBuilder<CommandSource> command = Commands
                 .literal(COMMAND)
-                .requires(cs -> cs.hasPermissionLevel(2))
+                .then(Commands
+                        .argument(PLAYER_NAME, EntityArgument.player())
+                        .executes(this::executeCommandForPlayer))
                 .then(Commands
                         .argument(DIM_ID, IntegerArgumentType.integer())
                         .then(Commands
@@ -43,30 +47,39 @@ public class TeleportMe {
                                         .argument(POS_Y, DoubleArgumentType.doubleArg())
                                         .then(Commands
                                                 .argument(POS_Z, DoubleArgumentType.doubleArg())
-                                                .executes(this::executeCommand)))));
+                                                .executes(this::executeCommandForPosition)))));
 
         event.getCommandDispatcher().register(command);
     }
 
-    private int executeCommand(CommandContext<CommandSource> ctx) throws CommandSyntaxException {
+    private int executeCommandForPlayer(CommandContext<CommandSource> ctx) throws CommandSyntaxException {
+        ServerPlayerEntity targetPlayer = EntityArgument.getPlayer(ctx, PLAYER_NAME);
+        ServerPlayerEntity serverPlayerEntity = ctx.getSource().asPlayer();
+
+        serverPlayerEntity.teleport((ServerWorld) targetPlayer.world, targetPlayer.posX, targetPlayer.posY, targetPlayer.posZ, serverPlayerEntity.rotationYaw, serverPlayerEntity.rotationPitch);
+
+        return 0;
+    }
+
+    private int executeCommandForPosition(CommandContext<CommandSource> ctx) throws CommandSyntaxException {
         int dimId = IntegerArgumentType.getInteger(ctx, DIM_ID);
 
-        double posX = DoubleArgumentType.getDouble(ctx, POS_X);
-        double posY = DoubleArgumentType.getDouble(ctx, POS_Y);
-        double posZ = DoubleArgumentType.getDouble(ctx, POS_Z);
-
-        DimensionType destinationDimension = StreamSupport
+        DimensionType dimensionType = StreamSupport
                 .stream(DimensionType.getAll().spliterator(), false)
                 .filter(dimType -> dimType.getId() == dimId)
                 .findAny()
                 .orElse(null);
 
-        if (destinationDimension == null) {
+        if (dimensionType == null) {
             return -1;
         }
 
+        double posX = DoubleArgumentType.getDouble(ctx, POS_X);
+        double posY = DoubleArgumentType.getDouble(ctx, POS_Y);
+        double posZ = DoubleArgumentType.getDouble(ctx, POS_Z);
+
         ServerPlayerEntity serverPlayerEntity = ctx.getSource().asPlayer();
-        ServerWorld destinationWorld = ctx.getSource().getServer().getWorld(destinationDimension);
+        ServerWorld destinationWorld = ctx.getSource().getServer().getWorld(dimensionType);
 
         serverPlayerEntity.teleport(destinationWorld, posX, posY, posZ, serverPlayerEntity.rotationYaw, serverPlayerEntity.rotationPitch);
 
